5756c92b233838d3ae69c79b9d3ec4ca066cbac9
shivam-pillow <shivam@pillow.fund>
2023-05-07 17:14:41 +0530

    Update profile feature fixed

--- src/middleware/numberCheck.js
+++ src/middleware/numberCheck.js
@@ -3,8 +3,9 @@ const User = require('../models/user-model')
 
 const numberCheck = async (req, res, next) => {
 
-    const contact1 =  req.user ? req.user.contact1:req.body.contact1
+    const contact1 =  req.body.contact1
     const user = await User.findOne({contact1 : contact1})
+    
     if(user){
         const message = "This number is already registered, please try with different number"
         if(req.user){
